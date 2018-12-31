21fbeda2651576763b6d9a9e5fa6faebfce97aa1
Qiuyun Chen <qiuyunchen@pursuit.org>
2018-12-31 04:42:01 -0500

    added display module to enhance the display of object on webpage

---
+++ services/display.js
@@ -0,0 +1,23 @@
+const displayObj = (obj) =>{
+    const str = JSON.stringify(obj);
+    const blockStyle = `style='margin-inline-start:20px;margin-block-start:0em;margin-block-end:0em'`;
+    return `{
+    <blockquote ${blockStyle}>
+        ${str.slice(1,-1)
+        .split('"').join('')
+        .split(':').join(': ')
+        .split(',').join(', ')
+        .split('#').join("'")
+        .split('Please fill').join("'Please fill")
+        .split('the student').join("the student'")
+        .split('class:').join("<br> class:")
+        .split('name: ').join("name: '")
+        .split(', age').join("', age")
+        .split('city: ').join("city: '")
+        .split(', grade').join("', grade")
+        }
+    </blockquote>
+    }`;
+}
+
+module.exports = {displayObj}

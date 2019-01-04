fa03d9686d906256547b254ac2be9a4e4fc13405
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-01-04 16:27:58 -0500

    finished add student path

--- index.js
+++ index.js
@@ -1,15 +1,73 @@
+const fs = require('fs');
+const {displayObj} = require('./services/display');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 // -------- path: default path
 app.get('/', (req, res)=>{
-    const {defaultHTML} = require('./default');
+    const {defaultHTML} = require('./services/default');
     res.send(defaultHTML);
 });
 
 // // -------- path: adding students to a class
-// app.get();
+app.get('/class/add', (req, res)=>{
+    const className = req.query.class;
+    const arrOfReqdKeys = ['name','age','city','grade'];
+    const success = {added: {}, class:`#${className}#`};
+    const error = {};
+    arrOfReqdKeys.forEach( key =>{
+        if (req.query[key]){
+            success.added[key] = req.query[key];
+        } else {
+            error.error = 'Please fill out all the information for the student';
+        }
+    });
+
+    fs.readFile(`./classes/${className}.json`, 'utf8', (err, data)=>{
+        if (err) { // need to create new class and save student to new class
+            if (error.error) {
+                res.send( displayObj(error) );
+            } else {
+                const thisClass = {students:[success.added]};
+                fs.writeFile(`./classes/${className}.json`, JSON.stringify(thisClass), (err) =>{
+                    if(err) {
+                        res.send('something went wrong and the student could not be added');
+                    } else {
+                        res.send( displayObj(success) );
+                    }
+                });
+            }
+        } else { // class already exists, just need to update class
+            if (error.error) {
+                res.send( displayObj(error) );
+            } else {
+                const thisClass = JSON.parse(data);
+                // check for existing student
+                const existingStud = thisClass.students.filter( s => s.name === success.added.name);
+                if (existingStud.length === 0){
+                    thisClass.students.push(success.added);
+                } else {
+                    thisClass.students.map(s => {
+                        if (s.name = success.added.name){
+                            s.age = success.added.age;
+                            s.city = success.added.city;
+                            s.grade = success.added.grade;
+                        }
+                    })
+                }
+                // update file
+                fs.writeFile(`./classes/${className}.json`, JSON.stringify(thisClass), err =>{
+                    if(err) {
+                        res.send('something went wrong and the student could not be added');
+                    } else {
+                        res.send( displayObj(success) );
+                    }
+                }); 
+            }
+        }
+    });
+});
 
 // // -------- path: list all students in a class
 // app.get();

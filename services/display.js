268dcc4b459f0b59a88cfadd32a1dc4756521d45
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-01-04 21:10:17 -0500

    made final adjustments to display module

--- services/display.js
+++ services/display.js
@@ -1,16 +1,14 @@
 const displayObj = (obj) =>{
     const str = JSON.stringify(obj);
     const blockStyle = `style='margin-inline-start:20px;margin-block-start:0em;margin-block-end:0em'`;
+    
     return `{
     <blockquote ${blockStyle}>
-        ${
-        str.slice(1,-1)
+        ${str.slice(1,-1)
         .split('"').join('')
         .split(':').join(': ')
         .split(',').join(', ')
         .split('#').join("'")
-        .split('Please fill').join("'Please fill")
-        .split('the student').join("the student'")
         .split('class:').join("<br> class:")
         .split('name: ').join("name: '")
         .split(', age').join("', age")
@@ -20,6 +18,8 @@ const displayObj = (obj) =>{
         .split(']').join('</blockquote> ]')
         .split('{name:').join('<br>{name:')
         .split('><br>{name:').join('>{name:')
+        .split('added: <br>').join('added: ')
+        .split(`[ <blockquote ${blockStyle}></blockquote> ]`).join('[ ]')
         }
     </blockquote>
     }`;
